4317d6d2f7d67c44e4ddaeb6bea98609cdad7fee
ngaswilly77 <ngaswilly77@gmail.com>
2020-07-22 09:33:04 +0100

    implement firebase utils, included ability to store authenticated users into firebase

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component'; 
+
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
+
+import './sign-up.styles.scss';
+
+class SignUp extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            displayName: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        };
+    }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const { displayName, email, password, confirmPassword } = this.state;
+
+        if(password !== confirmPassword) {
+            alert("Password don't match");
+            return;
+        }
+
+        try {
+            const { user } = await auth.createUserWithEmailAndPassword(email, password);
+
+            createUserProfileDocument(user, { displayName });
+            
+            this.state = {
+                displayName: '',
+                email: '',
+                password: '',
+                confirmPassword: ''
+            };
+        } catch(error) {
+            console.error(error);
+        }
+    }
+
+    handleChange = (event) => {
+        const { value, name } = event.target;
+
+        this.setState({[name]: value});
+    }
+
+    render() {
+        const { displayName, email, password, confirmPassword } = this.state;
+
+        return (
+            <div className="sign-up">
+                <h2 className="title">I do not have an account</h2>
+                <span>Sign up with your email and password</span>
+                <form className="sign-up-form" onSubmit={this.handleSubmit}>
+                    <FormInput
+                        name="displayName" 
+                        type="text"
+                        onChange={this.handleChange} 
+                        value={displayName} 
+                        required 
+                        label='Display Name'
+                    />
+                    <FormInput
+                        name="email" 
+                        type="email"
+                        onChange={this.handleChange} 
+                        value={email} 
+                        required 
+                        label='Email'
+                    />
+                    <FormInput
+                        name="password" 
+                        type="password"
+                        onChange={this.handleChange} 
+                        value={password} 
+                        required 
+                        label='Password'
+                    />
+                    <FormInput
+                        name="confirmPassword" 
+                        type="password"
+                        onChange={this.handleChange} 
+                        value={confirmPassword} 
+                        required 
+                        label='Confirm Password'
+                    />
+
+                    <CustomButton type="submit">Sign up</CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignUp;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,33 @@ const firebaseConfig = {
     appId: "1:893177151432:web:d105bcded7a2f793ce0afd"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData = {}) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData 
+            });
+        } catch(error) {
+            console.log('Error creating user', error.message);
+        }
+    }
+
+    return userRef;
+}
+
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
