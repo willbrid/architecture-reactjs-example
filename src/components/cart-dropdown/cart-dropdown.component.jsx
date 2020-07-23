065b2e8f31324d646893375b1aec95957a213ce1
ngaswilly77 <ngaswilly77@gmail.com>
2020-07-23 11:38:20 +0100

    add checkout page and chekcout item, create total cart selector

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,13 +3,14 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => {
+const CartDropdown = ({ cartItems, history, dispatch }) => {
     return (
         <div className="cart-dropdown">
             <div className="cart-items">
@@ -24,7 +25,10 @@ const CartDropdown = ({ cartItems, history }) => {
                     )        
                 }
             </div>
-            <CustomButton onClick={() => history.push('/checkout')}>
+            <CustomButton onClick={() => {
+                history.push('/checkout');
+                dispatch(toggleCartHidden());
+            }}>
                 GO TO CHECKOUT
             </CustomButton>
         </div>
