a0817282493dcd9d1b499d55f8e66b9431317ba0
ngaswilly77 <ngaswilly77@gmail.com>
2020-07-25 08:35:42 +0100

    integrate sop data from firestore and add WithSpinner HOC

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -40,6 +40,24 @@ export const createUserProfileDocument = async (userAuth, additionalData = {}) =
     return userRef;
 }
 
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+        const { title, items } = doc.data();
+
+        return {
+            routName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        }
+    });
+
+    return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    }, {});
+};
+
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
